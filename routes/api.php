0fafa87c8a67af54686f65031a2a4f2d0d1818c3
Ruel Rule <ruelrule05@gmail.com>
2025-05-10 12:25:15 +0800

    admin middleware created for rbac implementation

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\JwtAuthMiddleware;
 
 Route::middleware([JwtAuthMiddleware::class])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::middleware([AdminMiddleware::class])->group(function () {
+    Route::get('sample', function() {
+        dd('hello there admin!');
+    });
 });
 
 Route::post('login', [AuthController::class, 'login']);

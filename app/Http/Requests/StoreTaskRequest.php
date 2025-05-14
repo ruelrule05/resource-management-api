e027d7ac8165ae50223381d8fb43f99bb19b594e
Ruel Rule <ruelrule05@gmail.com>
2025-05-14 14:46:35 +0800

    updated validation rules

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->hasRole("administrator");
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required'],
+            'description' => ['nullable'],
+            'status' => ['required', Rule::in(['pending', 'to do', 'in progress', 'completed', 'on hold'])],
+            'project_id' => ['required', 'integer', 'exists:projects,id'],
+            'due_date' => ['nullable', 'date'],
+        ];
+    }
+}
